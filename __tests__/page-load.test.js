c760befa6f88e4a44cc7be88d7a0d1e4471b853d
tomitry <tomitry@gmail.com>
2019-08-16 18:58:14 +0300

    Step 2

--- __tests__/page-load.test.js
+++ __tests__/page-load.test.js
@@ -1,25 +1,62 @@
 import nock from 'nock';
 import os from 'os';
+import axios from 'axios';
 import { promises as fs } from 'fs';
 import path from 'path';
+import httpAdapter from 'axios/lib/adapters/http';
 import loadPage from '../src';
 
-const fixturesPath = './__fixtures__/';
+axios.defaults.adapter = httpAdapter;
+nock.disableNetConnect();
 
-const pathResolveFixtures = fileName => path.resolve(__dirname, `${fixturesPath}${fileName}`);
+const fixturesPath = './__fixtures__/';
+const resoursesPath = './hexlet-io-courses_files/';
+const pathResolveFixtures = fileName => path.resolve(__dirname, fixturesPath, fileName);
 
 test('loading page', async () => {
-  const testUrl = 'https://hexlet.io';
-  const fixtureFileName = 'hexlet-io-courses.html';
-  const expectedFileName = pathResolveFixtures(fixtureFileName);
-  nock(testUrl)
+  const originalHtmlFileName = pathResolveFixtures('original.html');
+  const expectedHtmlFileName = pathResolveFixtures('hexlet-io-courses.html');
+  const expectedCssFileName = pathResolveFixtures(`${resoursesPath}assets-application.css`);
+  const expectedImgFileName = pathResolveFixtures(`${resoursesPath}derivations-image`);
+  const expectedJsFileName = pathResolveFixtures(`${resoursesPath}packs-script.js`);
+
+  nock('https://hexlet.io')
     .get('/courses')
-    .replyWithFile(200, expectedFileName, {
+    .replyWithFile(200, originalHtmlFileName, {
       'Content-Type': 'application/html',
     });
+
+  nock('https://cdn2.hexlet.io')
+    .get('/assets/application.css')
+    .replyWithFile(200, expectedCssFileName, {
+      'Content-Type': 'text/css',
+    })
+    .get('/derivations/image')
+    .replyWithFile(200, expectedImgFileName, {
+      'Content-Type': 'image/jpeg',
+    })
+    .get('/packs/script.js')
+    .replyWithFile(200, expectedJsFileName, {
+      'Content-Type': 'application/javascript',
+    });
+
+
   const tempDir = await fs.mkdtemp(path.join(os.tmpdir(), 'page-loader'));
-  await loadPage(tempDir, `${testUrl}/courses`);
-  const actual = await fs.readFile(`${tempDir}/${fixtureFileName}`);
-  const expected = await fs.readFile(expectedFileName);
-  expect(actual).toStrictEqual(expected);
+  await loadPage(tempDir, 'https://hexlet.io/courses');
+
+  const actualHtml = await fs.readFile(`${tempDir}/hexlet-io-courses.html`, 'utf-8');
+  const expectedHtml = await fs.readFile(expectedHtmlFileName, 'utf-8');
+  expect(actualHtml).toMatch(expectedHtml);
+
+  const tempDirResourcesPath = path.resolve(tempDir, resoursesPath);
+  console.log(`temp resources path = ${tempDirResourcesPath}`);
+  const actualJs = await fs.readFile(`${tempDirResourcesPath}/packs-script.js`, 'utf-8');
+  const actualImg = await fs.readFile(`${tempDirResourcesPath}/derivations-image`);
+  const actualCss = await fs.readFile(`${tempDirResourcesPath}/assets-application.css`, 'utf-8');
+  const expectedJs = await fs.readFile(expectedJsFileName, 'utf-8');
+  const expectedImg = await fs.readFile(expectedImgFileName);
+  const expectedCss = await fs.readFile(expectedCssFileName, 'utf-8');
+  expect(actualCss).toEqual(expectedCss);
+  expect(actualJs).toEqual(expectedJs);
+  expect(actualImg).toStrictEqual(expectedImg);
 });
--- src/index.js
+++ src/index.js
@@ -1,14 +1,61 @@
 import axios from 'axios';
 import path from 'path';
 import { promises as fs } from 'fs';
+import cherio from 'cherio';
+import validUrl from 'valid-url';
+import url from 'url';
 
-const getFileName = url => url.substring(url.indexOf('//') + 2).replace(/[^a-zA-Z0-9]/g, '-');
+const getPageFileName = (pageUrl) => {
+  const parsedUrl = url.parse(pageUrl);
+  return `${parsedUrl.hostname}${parsedUrl.pathname}`.replace(/[^a-zA-Z0-9]/g, '-');
+};
+
+const getResourceFileName = (resourceUrl) => {
+  const parsedUrl = url.parse(resourceUrl);
+  const parsedPath = path.parse(parsedUrl.pathname);
+  const fileNamePart = path.join(parsedPath.dir, parsedPath.name);
+
+  return `${fileNamePart.substring(1).replace(/[^a-zA-Z0-9]/g, '-')}${parsedPath.ext}`;
+};
+
+const tag2url = {
+  script: 'src',
+  link: 'href',
+  img: 'src',
+};
+
+const getResourceUrls = ($, resourcesDir) => $('link, script, img')
+  .map((i, el) => {
+    const tag = tag2url[el.name];
+    const resourceUrl = $(el).attr(tag);
+    if (resourceUrl && validUrl.isUri(resourceUrl)) {
+      const resourcePath = `./${path.join('./', resourcesDir, getResourceFileName(resourceUrl))}`;
+      $(el).attr(tag, resourcePath);
+      return resourceUrl;
+    }
+    return '';
+  })
+  .get()
+  .filter(el => el !== '');
 
 export default (destinationDirectory, pageAddress) => {
-  const fileName = path.join(destinationDirectory, `${getFileName(pageAddress)}.html`);
+  const fileName = path.join(destinationDirectory, `${getPageFileName(pageAddress)}.html`);
   return axios.get(pageAddress)
-    .then(res => res.data)
-    .then(data => fs.writeFile(fileName, data))
+    .then((htmlResp) => {
+      const $ = cherio.load(htmlResp.data);
+      const resourcersRelativePath = `${getPageFileName(pageAddress)}_files`;
+      const resourcesAbsolutPath = path.join(destinationDirectory, resourcersRelativePath);
+      const urls = getResourceUrls($, resourcersRelativePath);
+      return fs.mkdir(resourcesAbsolutPath)
+        .then(() => fs.writeFile(fileName, $.html()))
+        .then(() => Promise.all(urls.map(el => axios.get(el, { responseType: 'arraybuffer' })
+          .then((resourceResp) => {
+            const resourceFileName = path.join(
+              resourcesAbsolutPath, getResourceFileName(el),
+            );
+            return fs.writeFile(resourceFileName, resourceResp.data);
+          }))));
+    })
     .catch((err) => {
       console.log(err);
       throw err;
